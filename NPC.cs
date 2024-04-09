177340842534fe421cdf4d5eda7c5c3b3e1fd747
robjam1990 <robjam1990@live.ca>
2024-04-08 21:28:18 -0400

    Updates and additions

---
+++ NPC.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Initialization
+{
+    class Player
+    {
+        public string ?Name { get; }
+        public string ?Behavior { get; private set; }
+        public bool IsHungry { get; set; }
+        public bool IsThirsty { get; set; }
+        public bool IsTired { get; set; }
+        public bool IsDead { get; set; }
+        public int Age { get; set; }
+        public int Health { get; set; } = 100;
+        public int Hunger { get; set; } = 100;
+        public int Thirst { get; set; }
+        public int Energy { get; set; }
+        public int DaysAlive { get; set; }
+        public int DaysSinceEaten { get; set; }
+        public int DaysSinceDrank { get; set; }
+        public int DaysSinceSlept { get; set; }
+
+    }
+}
--- TUI.cs
+++ TUI.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DocumentFormat.OpenXml.Drawing;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,22 +11,76 @@ namespace Initialization
 {
     internal interface ITUI
     {
+        void FullUI();
+        void NotificationUI();
+        void MessageUI();
+        void ActionUI();
+        void InventoryUI();
+        void CharacterStatsUI();
+        void MapUI();
+        void QuestsUI();
+
+
         class UI()
         {
+            public void FullUI()
+            {
+                WriteLine(@"   ________________________________________________________________________________________________________________   ");
+                WriteLine(@" /__==========================================/\==================================================================__\ ");
+                WriteLine(@"/|Text input:                                 ||Notifications:                                                      |\");
+                WriteLine(@"||                                            ||                                                                    ||");
+                WriteLine(@"||                                            ||                                                                    ||");
+                WriteLine(@"||                                            ||                                                                    ||");
+                WriteLine(@"||                                            ||                                                                    ||");
+                WriteLine(@"||                                            ||                                                                    ||");
+                WriteLine(@"||                                            ||                                                                    ||");
+                WriteLine(@"||                                            ||                                                                    ||");
+                WriteLine(@"||                                            ||                                                                    ||");
+                WriteLine(@"|__===========================================\/==========================================_/======================__/|");
+                WriteLine(@"||(M)ovement        (O)bservation      (R)un  /                                           |\_  Character Quests:  _/_|");
+                WriteLine(@"||                                           /                                            |_\|\__|__=============__\_|");
+                WriteLine(@"||                             __=========__|_____________________________________________|__\_|/|List:             ||");
+                WriteLine(@"||                             |Text output:|                                                 \ \|                  ||");
+                WriteLine(@"||(C)onversation:              |__========__\                                                  \ |                  ||");
+                WriteLine(@"||_____________________________|_____________\__________________________________________________\|                  ||");
+                WriteLine(@"|__============================================================================================__|                  ||");
+                WriteLine(@"|/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\|                  ||");
+                WriteLine(@"||Inventory:                                  ||Map(7*8):                                        |                  ||");
+                WriteLine(@"||                                            ||                                                 |                  ||");
+                WriteLine(@"||                                            ||                                                 |                  ||");
+                WriteLine(@"||                                            ||                                                 |                  ||");
+                WriteLine(@"||                                            ||                                                 |                  ||");
+                WriteLine(@"||                                            ||                                                 |                  ||");
+                WriteLine(@"||                                            ||                                                 |                  ||");
+                WriteLine(@"||                                            ||                                                 |                  /|");
+                WriteLine(@"||                                            |\                                                 |                 / |");
+                WriteLine(@"||                                            |__\===================/===========================^================/__|");
+                WriteLine(@"||                                            ||Character Statistics/                                               ||");
+                WriteLine(@"||                                            |__================__/                                                ||");
+                WriteLine(@"||                                            ||/                                                                   ||");
+                WriteLine(@"||                                            ||                                                                    ||");
+                WriteLine(@"||                                            ||                                                                    ||");
+                WriteLine(@"||                                            ||                                                                    ||");
+                WriteLine(@"||                                            ||                                                                    ||");
+                WriteLine(@"|\                                            ||                                                                    /|");
+                WriteLine(@"| \                                           /\                                                                   / |");
+                WriteLine(@"|__\=========================================/==\=================================================================/__|");
+            }
+
             public void NotificationUI()
             {
-            WriteLine(@"/\==================================================================__\ ");
-            WriteLine(@"||Notifications:                                                      |\");
-            WriteLine(@"||                                                                    ||");
-            WriteLine(@"||                                                                    ||");
-            WriteLine(@"||                                                                    ||");
-            WriteLine(@"||                                                                    ||");
-            WriteLine(@"||                                                                    ||");
-            WriteLine(@"||                                                                    ||");
-            WriteLine(@"||                                                                    ||");
-            WriteLine(@"||                                                                    ||");
-            WriteLine(@"\/==========================================_/======================__/|");
-            WriteLine(@"");
+                WriteLine(@"/\==================================================================__\ ");
+                WriteLine(@"||Notifications:                                                      |\");
+                WriteLine(@"||                                                                    ||");
+                WriteLine(@"||                                                                    ||");
+                WriteLine(@"||                                                                    ||");
+                WriteLine(@"||                                                                    ||");
+                WriteLine(@"||                                                                    ||");
+                WriteLine(@"||                                                                    ||");
+                WriteLine(@"||                                                                    ||");
+                WriteLine(@"||                                                                    ||");
+                WriteLine(@"\/==========================================_/======================__/|");
+                WriteLine(@"");
             }
 
             public void MessageUI()
@@ -142,3 +198,4 @@ namespace Initialization
         }
     }
 }
+
